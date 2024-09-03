8b9d29e387e3c26f5babe413dafd71bdec20c3e7
Maria Kazakova <mariakazakova92@gmail.com>
2024-09-03 12:29:53 +0300

    added routing

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,26 @@ import reportWebVitals from './reportWebVitals';
 import App from './app/App';
 import {Provider} from 'react-redux';
 import {store} from './app/store';
+import {createBrowserRouter} from "react-router-dom";
+import {Login} from "./features/Login/Login";
+import {TodolistsList} from "./features/TodolistsList/TodolistsList";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        children: [
+            {
+                path: "/login",
+                element: <Login/>,
+            },
+            {
+                path: "/todolists",
+                element: <TodolistsList/>,
+            },
+        ],
+    },
+]);
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
